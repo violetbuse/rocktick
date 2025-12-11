20662b939d2b6b6da86b6df4a4f33ea208c16c04
Violet Buse <violet@git.violetbuse.com>
2025-12-11 10:31:56 +0100

    changed build script

--- build.rs
+++ build.rs
@@ -1,9 +1,20 @@
+use std::process::Command;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=Cargo.toml");
     println!("cargo:rerun-if-changed=proto");
     tonic_prost_build::compile_protos("proto/broker.proto")?;
 
     println!("cargo:rerun-if-changed=migrations");
+    // println!("cargo:rerun-if-changed=src");
+    // let status = Command::new("cargo")
+    //     .args(["sqlx", "prepare"])
+    //     .status()
+    //     .expect("Failed to run sqlx prepare");
+
+    // if !status.success() {
+    //     panic!("sqlx prepare failed");
+    // }
 
     Ok(())
 }
